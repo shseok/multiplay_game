cf80ab9d6a66d392214d3cbc84599082d3445d42
shseok <gus0674@ajou.ac.kr>
2022-05-11 23:36:21 +0900

    Create Room Screen UI

--- lib/screens/create_room_screen.dart
+++ lib/screens/create_room_screen.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:mp_game/responsive/responsive.dart';
+import 'package:mp_game/widgets/custom_textfield.dart';
+
+import '../widgets/custom_button.dart';
+import '../widgets/custom_text.dart';
 
 class CreateRoomScreen extends StatefulWidget {
   static String routeName = '/create-room';
@@ -9,8 +14,42 @@ class CreateRoomScreen extends StatefulWidget {
 }
 
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
+
+  final TextEditingController _nameController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+
+    final size = MediaQuery.of(context).size;
+    print(size.height);
+
+    return Responsive(
+      child: Scaffold(
+        body: Container(
+          margin: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              CustomText(
+                shadows: [
+                  Shadow(
+                    blurRadius: 40,
+                    color: Colors.blue,
+                  ),
+                ],
+                text: '방 만들기',
+                fontSize: 70,
+              ),
+              SizedBox(height: size.height * 0.08),
+              CustomTextField(controller: _nameController,
+                hintText: '닉네임을 입력해주세요',),
+              SizedBox(height: size.height * 0.03),
+              CustomButton(onTap: (){}, text: '생성'),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
