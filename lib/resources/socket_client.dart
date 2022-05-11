45705b3cc6c51a93b3d36ee878aeb8ca5e540bc5
shseok <gus0674@ajou.ac.kr>
2022-05-12 03:23:46 +0900

    Connecting to SocketIO

---
+++ lib/resources/socket_client.dart
@@ -0,0 +1,21 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+import '../account/account.dart';
+
+class SocketClient{ // single tone pattern
+  IO.Socket? socket;
+  static SocketClient? _instance;
+  
+  SocketClient._internal(){
+    socket = IO.io(uri, <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    socket!.connect();
+  }
+
+  static SocketClient get instance{
+    _instance ??= SocketClient._internal();
+    return _instance!;
+  }
+}
